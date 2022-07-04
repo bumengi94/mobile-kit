63ffe83ceb389bd5afa2d908265ebe546b2dbbf7
Buğra Mengi <bumengi94@gmail.com>
2022-07-04 09:55:35 +0300

    app added

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const App: FC = () => (
+	<SafeAreaView>
+		<Text>Mobile Kit</Text>
+	</SafeAreaView>
+);
+
+export default App;
