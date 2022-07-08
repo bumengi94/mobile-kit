524ae70a1dfe59975f4bd42dcbfdf3d49b8be495
Buğra Mengi <bumengi94@gmail.com>
2022-07-09 01:31:00 +0300

    navigation setup added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React, { FC, lazy, StrictMode, Suspense } from "react";
+import React, { FC, lazy, Suspense } from "react";
 import { Provider } from "react-redux";
 import { store } from "./redux";
 import { Loading } from "./components";
+import { NavigationContainer } from "@react-navigation/native";
 
-const Test = lazy(() => import("./components/Test"));
+const MainRouter = lazy(() => import("./routers"));
 
 const App: FC = () => {
 	return (
-		<StrictMode>
-			<Suspense fallback={<Loading />}>
-				<Provider store={store}>
-					<Test />
-				</Provider>
-			</Suspense>
-		</StrictMode>
+		<Suspense fallback={<Loading />}>
+			<Provider store={store}>
+				<NavigationContainer>
+					<MainRouter />
+				</NavigationContainer>
+			</Provider>
+		</Suspense>
 	);
 };
 
---
+++ src/routers/Main.tsx
@@ -0,0 +1,16 @@
+import React, { FC, lazy } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Test = lazy(() => import("../components/Test"));
+
+const { Navigator, Screen } = createStackNavigator();
+
+const MainRouter: FC = () => {
+	return (
+		<Navigator>
+			<Screen name={"Test"} component={Test} />
+		</Navigator>
+	);
+};
+
+export default MainRouter;
